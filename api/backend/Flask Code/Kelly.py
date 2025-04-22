65235424e3b138e0eb73f260653517a90375c560
levchen18 <chen.lev@northeastern.edu>
2025-04-22 16:17:37 -0700

    Flask Code for Persona 3 Kelly completed

--- api/backend/Flask Code/Kelly.py
+++ api/backend/Flask Code/Kelly.py
@@ -11,11 +11,11 @@ from backend.ml_models.model01 import predict
 #------------------------------------------------------------
 # Create a new Blueprint object, which is a collection of
 # routes.
-library_donation_supplier_Kelly = Blueprint('Kelly', __name__)
+kelly = Blueprint('Kelly', __name__)
 
 #------------------------------------------------------------
 # Get all the library donation guidelines to the library from the system
-@library_donation_guidelines.route('/guidelines')
+@books.route('/guidelines')
 def book_donations_guidelines():
     query = '''
     SELECT WebsiteURL, Name, LastUpdated
@@ -32,7 +32,7 @@ def book_donations_guidelines():
     return response
 #------------------------------------------------------------
 #Get all the books donated to the library from the system
-@donated_books.route('/allBooks', methods=['GET'])
+@books.route('/allBooks', methods=['GET'])
 def user_donated_books():
     query = '''
             SELECT BookID,
@@ -50,3 +50,56 @@ def user_donated_books():
     response.status_code = 200
     return response
 # ------------------------------------------------------------
+# Update library book info for books donated to the library
+@books.route('/updateBookStatus', methods = ['PUT'])
+def update_library_books():
+    current_app.logger.info('PUT /book route')
+    book_info = request.json
+    book_id = book_info['id']
+    title = book_info['title']
+    genre = book_info['genre']
+    pages = book_info['pages']
+    query = '''
+                INSERT INTO Books (book_id, title, genre, pages)
+                VALUES (%s, %s, %s, %s, %s)
+                '''
+    data = (book_id, title, genre, pages)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'New book added to the library!'
+#------------------------------------------------------------------
+#Delete the book genre
+@books.route('/deleteBooks', methods = ['DELETE'])
+def delete_book_genre():
+    current_app.logger.info('DELETE /removeBooks route')
+    query = '''
+    DELETE FROM Books
+    WHERE Genre = {genre}'''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Old Book Genre Deleted!'
+#-----------------------------------------------------------------------
+#Librarian's favorite customer
+@book.route('/favoriteCustomer', methods = ['POST'])
+def favorite_customer():
+    current_app.logger.info('POST /events route')
+
+    customer_fav = request.json
+    FName = customer_fav['FName']
+    MName = customer_fav['MName']
+    LName =customer_fav['LName']
+    CustomerID = customer_fav['CustomerID']
+    Membership_Date = customer_fav['Membership_Date']
+    query = '''
+               INSERT INTO Events (FName, MName, LName, CustomerID, Membership_Date)
+               VALUES (%s, %s, %s, %s, %s)
+               '''
+    data = (FName, MName, LName, CustomerID, Membership_Date)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+
+    return 'Favorite Member of the library created', 201
