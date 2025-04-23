041753dff9f04b77f486171fee8b118bce3fbfa0
levchen18 <chen.lev@northeastern.edu>
2025-04-23 13:47:01 -0700

    Kelly pre-coding file creation

--- app/src/pages/20_Admin_Home.py
+++ app/src/pages/20_Admin_Home.py
@@ -9,9 +9,22 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('System Admin Home Page')
+st.title(f"Welcome Admin, {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
 
-if st.button('Update ML Models', 
-             type='primary',
+if st.button('View the favorite customer at this library',
+             type = 'primary',
+             use_container_width = True):
+  st.switch_page('pages/22_Favorite_Customer.py')
+
+if st.button('View the book donation guidelines',
+             type = 'primary',
              use_container_width=True):
-  st.switch_page('pages/21_ML_Model_Mgmt.py')
+  st.switch_page('pages/23_book_donation_guidelines.py')
+
+if st.button('Update the books in the library',
+             type = 'primary',
+             use_container_width=True):
+  st.switch_page('pages/24_update_library_books.py')
