e26d3155673e176be767289f5c530e86bc23c6f3
Andrew Chen <andrewchen9998@gmail.com>
2025-04-24 14:15:34 -0700

    Emily Persona Streamlit Button

--- app/src/Home.py
+++ app/src/Home.py
@@ -73,5 +73,12 @@ if st.button('Act as System Administrator',
     st.session_state['first_name'] = 'SysAdmin'
     st.switch_page('pages/20_Admin_Home.py')
 
+if st.button('Act as Librarian',
+            type = 'primary',
+            use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'librarian'
+    st.session_state['first_name'] = 'Emily'
+    st.switch_page('pages/05_Librarian_Home.py')
 
 
